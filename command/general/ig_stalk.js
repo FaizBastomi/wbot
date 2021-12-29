d36c041ad98a12f7ccf6e68c7c17adb772257fca
FaizBastomi <faizbastomy3@gmail.com>
2021-12-29 22:50:37 +0700

    fix igstalk

--- command/general/ig_stalk.js
+++ command/general/ig_stalk.js
@@ -10,7 +10,7 @@ module.exports = {
             if (!args.length > 0) return await sock.sendMessage(msg.from, { text: "Instagram username required!" }, { quoted: msg });
             const profile = await igProfile(args[0]);
             let text = Object.keys(profile.metadata).map((str) => `${str}: ${profile.metadata[str]}`).join('\n');
-            await sock.sendMessage(msg.from, { image: profile.picUrl.hd, caption: text }, { quoted: msg });
+            await sock.sendMessage(msg.from, { image: { url: profile.picUrl.hd }, caption: text }, { quoted: msg });
         }
         catch (e) {
             await sock.sendMessage(msg.from, { text: `${e}` }, { quoted: msg });
