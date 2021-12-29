8e75f05823e12d1f4a87b35cb69d0da8230ce1e5
FaizBastomi <faizbastomy3@gmail.com>
2021-12-29 21:49:30 +0700

    fix: fbdl.js

--- command/downloader/fbdl.js
+++ command/downloader/fbdl.js
@@ -10,6 +10,7 @@ module.exports = {
     desc: "Download Facebook video",
     async exec(msg, sock, args) {
         try {
+            if (!args.length > 0) return await sock.sendMessage(msg.from, { text: "No url provided" }, { quoted: msg })
             let data = await fbdl(args[0])
 
             if (data.length === 0) return await sock.sendMessage(msg.from, { text: `ID:\n${lang.indo.util.download.fbPriv}\n\nEN:\n${lang.eng.util.download.fbPriv}` }, { quoted: msg })
