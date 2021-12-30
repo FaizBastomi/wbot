197009e372c55c94997d453bb8952ab309e17932
FaizBastomi <faizbastomy3@gmail.com>
2021-12-30 21:33:18 +0700

    update help.js

--- command/general/help.js
+++ command/general/help.js
@@ -10,7 +10,7 @@ module.exports = {
             const name = args[0].toLowerCase();
             const { commands, prefix } = djs;
             const cmd = commands.get(name) || commands.find((cmd) => cmd.alias && cmd.alias.includes(name));
-            if (!cmd) return await sock.sendMessage(msg.from, { text: "No command found" }, { quoted: msg });
+            if (!cmd || cmd.category === "private") return await sock.sendMessage(msg.from, { text: "No command found" }, { quoted: msg });
             else data.push(cmd.name);
             if (cmd.alias) data.push(`*Alias:* ${cmd.alias.join(', ')}`);
             if (cmd.desc) data.push(`*Description:* ${cmd.desc}`);
