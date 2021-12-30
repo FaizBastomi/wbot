3d40648a257f79c021e717f49e81861e2d655dd4
FaizBastomi <faizbastomy3@gmail.com>
2021-12-31 00:09:52 +0700

    feat: stiker with text

---
+++ command/general/sticker_meme.js
@@ -0,0 +1,42 @@
+const { downloadMedia, fetchBuffer } = require("../../utils");
+const { memeText } = require("../../utils/uploader");
+const { sticker } = require("../../lib/convert");
+const lang = require("../other/text.json");
+
+module.exports = {
+    name: "stikmeme",
+    alias: ['stickermeme', 'stikermeme', 'stiktext', 'stickertext', 'stikertext', 'sticktext'],
+    category: "general",
+    desc: "Create a sticker from image",
+    async exec(msg, sock, args, arg) {
+        const { quoted, from, type } = msg;
+
+        const content = JSON.stringify(quoted);
+        const isMedia = type === 'imageMessage';
+        const isQImg = type === 'extendedTextMessage' && content.includes('imageMessage');
+        const isQDoc = type === 'extendedTextMessage' && content.includes('documentMessage');
+        let top = arg.split('|')[0] || '';
+        let bottom = arg.split('|')[1] || '';
+
+        try {
+            if ((isMedia && !msg.message.videoMessage) || isQImg) {
+                const media = isQImg ? quoted.message : msg.message;
+                const buffer = await downloadMedia(media);
+                let memeImg = await memeText(buffer, top, bottom);
+                const stickerBuffer = await sticker(memeImg, { isImage: true, cmdType: "1" });
+                await sock.sendMessage(from, { sticker: stickerBuffer }, { quoted: msg });
+            } else if (
+                isQDoc && (/image/.test(quoted.message.documentMessage.mimetype))
+            ) {
+                const buffer = await downloadMedia(quoted.message);
+                let memeImg = await memeText(buffer, top, bottom);
+                const stickerBuffer = await sticker(memeImg, { isImage: true, cmdType: "1" });
+                await sock.sendMessage(from, { sticker: stickerBuffer }, { quoted: msg });
+            } else {
+                await sock.sendMessage(from, { text: `IND:\n${lang.indo.stickmeme}\n\nEN:\n${lang.eng.stickmeme}` }, { quoted: msg });
+            }
+        } catch (e) {
+            await sock.sendMessage(from, { text: "Error while creating sticker" }, { quoted: msg });
+        }
+    }
+}
--- utils/uploader.js
+++ utils/uploader.js
@@ -1,7 +1,9 @@
 const axios = require("axios").default;
 const cheerio = require("cheerio");
 const BodyForm = require("form-data");
-const { createReadStream } = require("fs");
+const { fromBuffer } = require("file-type");
+const { fetchBuffer } = require("./index");
+const { createReadStream, writeFile, unlinkSync } = require("fs");
 
 const webp2mp4 = (path) => {
     return new Promise((resolve, reject) => {
@@ -43,6 +45,48 @@ const webp2mp4 = (path) => {
     })
 }
 
+/**
+ * Upload to Telegra.ph
+ * @param {Buffer} fileData 
+ * @returns {Promise<URL|string>}
+ */
+const upTgph = (fileData) => {
+    return new Promise(async (resolve, reject) => {
+        const { ext } = await fromBuffer(fileData);
+        const filePath = 'utils/tmp.' + ext;
+        writeFile(filePath, fileData, async (err) => {
+            if (err) unlinkSync(filePath) && reject(err);
+            console.log('Uploading to telegra.ph...')
+            const form = new BodyForm();
+            form.append('file', createReadStream(filePath));
+            const { data } = await axios({ url: "https://telegra.ph/upload", data: form, method: "post", responseType: "json", headers: { ...form.getHeaders() } }).catch(reject);
+            if (data.error) unlinkSync(filePath) && reject(data.error);
+            console.log('Success');
+            resolve('https://telegra.ph' + data[0].src);
+            unlinkSync(filePath);
+        })
+    })
+}
+
+/**
+ * memeText -- add text to image
+ * @param {Buffer} imageData 
+ * @param {string} top 
+ * @param {string} bottom 
+ * @returns {Promise<Buffer>}
+ */
+const memeText = (imageData, top, bottom) => new Promise(async (resolve, reject) => {
+    if (!imageData) reject('No imageData');
+    const imageUrl = await upTgph(imageData).catch(reject);
+    let topText = top.trim().replace(/\s/g, '_').replace(/\?/g, '~q').replace(/\%/g, '~p').replace(/\#/g, '~h').replace(/\//g, '~s');
+    let bottomText = bottom.trim().replace(/\s/g, '_').replace(/\?/g, '~q').replace(/\%/g, '~p').replace(/\#/g, '~h').replace(/\//g, '~s');
+
+    let result = `https://api.memegen.link/images/custom/${topText}/${bottomText}.png?background=${imageUrl}`;
+    const binResult = await fetchBuffer(result);
+    resolve(binResult);
+})
 module.exports = {
-    webp2mp4
+    webp2mp4,
+    upTgph,
+    memeText
 }
