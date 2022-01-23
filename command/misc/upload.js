4e8f56515be0773c6306e3c67cb807e31e914f6f
FaizBastomi <faizbastomy3@gmail.com>
2022-01-23 20:13:43 +0700

    docs: update 'upload' usage

--- command/misc/upload.js
+++ command/misc/upload.js
@@ -3,8 +3,8 @@ const { uploaderAPI } = require("../../utils/uploader");
 module.exports = {
     name: "upload",
     alias: ["upld"],
-    use: "Send/Reply to a message media with caption\n!upload *<hosting>*\n\n"
-        + "- telegraph\n- uguu\n- anonfiles",
+    use: "*<hosting>*\nSend/Reply to a message media with caption\n\n"
+        + "*Hosting*\n- telegraph\n- uguu\n- anonfiles",
     category: "misc",
     async exec(msg, sock, args) {
         const { from, quoted } = msg;
@@ -14,10 +14,10 @@ module.exports = {
 
             if (quoted) {
                 const resUrl = await uploaderAPI((await quoted.download()), host);
-                await sock.sendMessage(from, { text: `*Host:* ${resUrl.host}\n*URL:* ${resUrl.data.url}\n*Name:* ${resUrl.data.name}\n*Size:* ${resUrl.data.size}` });
+                await sock.sendMessage(from, { text: `*Host:* ${resUrl.host}\n*URL:* ${resUrl.data.url}\n*Name:* ${resUrl.data.name}\n*Size:* ${resUrl.data.size}` }, { quoted: msg });
             } else {
                 const resUrl = await uploaderAPI((await msg.download()), host);
-                await sock.sendMessage(from, { text: `*Host:* ${resUrl.host}\n*URL:* ${resUrl.data.url}\n*Name:* ${resUrl.data.name}\n*Size:* ${resUrl.data.size}` });
+                await sock.sendMessage(from, { text: `*Host:* ${resUrl.host}\n*URL:* ${resUrl.data.url}\n*Name:* ${resUrl.data.name}\n*Size:* ${resUrl.data.size}` }, { quoted: msg });
             }
         } catch(e) {
             await msg.reply(`Error: ${e.message}`);
