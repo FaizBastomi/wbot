2c4d79d73769d784106285837f6da5c0c3a9178e
FaizBastomi <faizbastomy3@gmail.com>
2022-01-31 21:33:32 +0700

    ⚗️ feat: translate feature

---
+++ command/misc/tr.js
@@ -0,0 +1,29 @@
+const trAPI = require("@vitalets/google-translate-api");
+
+
+module.exports = {
+    name: "tr",
+    alias: ["translate"],
+    category: "misc",
+    use: "<text> atau reply sebuah pesan teks",
+    async exec(msg, _, args) {
+        const { quoted } = msg;
+        try {
+            let text;
+            if (quoted) {
+                text = await translate(quoted.text, args[0]);
+                await msg.reply(text);
+            } else {
+                text = await translate(args.slice(1).join(" "), args[0]);
+            }
+        } catch (e) { await msg.reply(e) }
+    }
+}
+
+async function translate(text, lang) {
+    let text2;
+    try {
+        text2 = (await trAPI(text, { client: "gtx", to: lang })).text;
+    } catch(e) { throw "Failed to translate" }
+    finally { return text2; }
+}
