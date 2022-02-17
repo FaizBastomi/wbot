0b6481a807c4e8034d425a7eee89320104e095db
FaizBastomi <faizbastomy3@gmail.com>
2022-02-17 13:04:33 +0700

    forget

--- command/downloader/yts.js
+++ command/downloader/yts.js
@@ -10,7 +10,7 @@ module.exports = {
         if (args.length < 1) return await msg.reply('No query given to search.');
         const ytsData = await yts(args.join(' '), 'long')
         let txt = `YouTube Search\n   ~> Query: ${args.join(' ')}\n`
-        for (let i = 0; i < r.length; i++) {
+        for (let i = 0; i < ytsData.length; i++) {
             txt += `\n📙 Title: ${ytsData[i].title}\n📎 Url: ${ytsData[i].url}\n🚀 Upload: ${ytsData[i].ago}\n`
         }
         await sock.sendMessage(msg.from, { image: { url: ytsData[0].image }, caption: txt }, { quoted: msg })
