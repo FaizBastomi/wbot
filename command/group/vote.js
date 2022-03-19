ec254911e37dd024e5b585e99f7191e11aa56810
FaizBastomi <faizbastomy3@gmail.com>
2022-03-19 21:05:50 +0700

    ✨ Updates * edit vote.js * new: votes.js * now using class

--- command/group/vote.js
+++ command/group/vote.js
@@ -1,4 +1,5 @@
-let voteMap = new Map();
+const UtilVotes = require("../../event/database/votes");
+const vote = new UtilVotes();
 
 module.exports = {
     name: "vote",
@@ -13,17 +14,18 @@ module.exports = {
         if (!args.length > 0) return await msg.reply("Please, read availabe option at #help vote");
 
         let voteState = args[0],
-            voteData = voteMap.get(from),
-            isVotePresent = voteMap.has(from),
-            status, sections;
+            voteData = vote.show(from),
+            sections, status = "";
 
         try {
             if (voteState === "create") {
-                if (isVotePresent) return await msg.reply(
+                if (voteData.present) return await msg.reply(
                     "There is already vote room in this group\nplease, delete current room."
                 )
-                makeRoom(from, "create_room", sender, args.splice(1).join(" "));
-                if (voteMap.has(from)) {
+                vote.create(from, sender, args.splice(1).join(" "));
+                if (
+                    (vote.show(from)).present
+                ) {
                     sections = [{
                         title: "Select Options", rows: [
                             { title: "Upvote", description: "Upvote this vote", rowId: "#vote up" },
@@ -42,8 +44,8 @@ module.exports = {
                 }
             }
             else if (voteState === "up") {
-                if (!isVotePresent) return await msg.reply("Please, create a vote room first before.\nExample: *!vote create*");
-                status = updateVote(from, sender, "up");
+                if (!voteData.present) return await msg.reply("Please, create a vote room first before.\nExample: *!vote create*");
+                status = vote.upvote(from, sender);
                 if (status === "added") {
                     await msg.reply("You voted.");
                 } else if (status === "voted") {
@@ -51,20 +53,21 @@ module.exports = {
                 }
             }
             else if (voteState === "down") {
-                if (!isVotePresent) return await msg.reply("Please, create a vote room first before.\nExample: *!vote create*");
-                status = updateVote(from, sender, "down");
-                if (status === "vote_down") {
+                if (!voteData.present) return await msg.reply("Please, create a vote room first before.\nExample: *!vote create*");
+                status = vote.downvote(from, sender);
+                if (status === "added") {
                     await msg.reply("Success downvote current active vote.");
-                } else if (status === "down_voted") {
+                } else if (status === "downvoted") {
                     await msg.reply("You already down voted this vote.");
                 }
             }
             else if (voteState === "show") {
-                if (!isVotePresent) return await msg.reply("Please, create a vote room first before.\nExample: *!vote create*");
+                if (!voteData.present) return await msg.reply("Please, create a vote room first before.\nExample: *!vote create*");
                 // 
+                voteData = voteData.data;
                 let text = `Current Group Vote (${voteData["name"]})\nOwner: ${voteData["owner"].split("@")[0]}\n\n`
-                    + `Upvote:\n- ${voteData["voted"].map(v => `@${v.split("@")[0]}`).join("\n- ")}\n\n`
-                    + `Downvote:\n- ${voteData["down"].map(v => `@${v.split("@")[0]}`).join("\n- ")}\n`
+                    + `Upvote:\n- ${voteData["upvote"].map(v => `@${v.split("@")[0]}`).join("\n- ")}\n\n`
+                    + `Downvote:\n- ${voteData["downvote"].map(v => `@${v.split("@")[0]}`).join("\n- ")}\n`
                 sections = [{
                     title: "Select Options", rows: [
                         { title: "Upvote", description: "Upvote this vote", rowId: "#vote up" },
@@ -77,8 +80,8 @@ module.exports = {
                 await sock.sendMessage(from, {
                     text,
                     mentions: [
-                        ...voteData["voted"],
-                        ...voteData["down"]
+                        ...voteData["upvote"],
+                        ...voteData["downvote"]
                     ],
                     footer: "Kaguya PublicBot • FaizBastomi",
                     buttonText: "Vote",
@@ -86,121 +89,26 @@ module.exports = {
                 }, { quoted: msg });
             }
             else if (voteState === "del_vote") {
-                if (!isVotePresent) return await msg.reply("Please, create a vote room first before.\nExample: *!vote create*");
-                status = updateVote(from, sender, "delete");
-                if (status === "vote_del") {
+                if (!voteData.present) return await msg.reply("Please, create a vote room first before.\nExample: *!vote create*");
+                status = vote.delete_vote(from, sender);
+                if (status === "deleted") {
                     await msg.reply("Deleted your vote from current room.");
                 }
-                else if (status === "no_data") {
+                else if (status === "no_vote") {
                     await msg.reply("You never participate in current vote room.");
                 }
             }
             else if (voteState === "del_room") {
-                if (!isVotePresent) return await msg.reply("No active vote room in this group.");
-                if (voteData["owner"] !== sender) return await msg.reply("You're not the room owner.");
-                status = makeRoom(from, "delete_room");
-                if (status === "deleted") {
+                if (!voteData.present) return await msg.reply("No active vote room in this group.");
+                if (voteData["data"]["owner"] !== sender) return await msg.reply("You're not the room owner.");
+                status = vote.delete(from, sender);
+                if (status) {
                     await msg.reply("Room deleted.");
                 }
             }
             voteData = null;
         } catch (e) {
             await msg.reply(`Error: ${e.message}`);
-        }
-    }
-}
-
-/**
- * Make new room for vote
- * @param {string} roomId group current id
- * @param {"create_room"|"delete_room"} status status
- * @param {string} roomOwner room creator number/id
- */
-const makeRoom = (roomId, status, roomOwner, name = "Default Room") => {
-    if (status === "create_room") {
-        if (name === "") name = "Default Room";
-        voteMap.set(roomId, { owner: roomOwner, name, voted: [], down: [] })
-        return "new_room";
-    }
-    else if (status === "delete_room") {
-        voteMap.delete(roomId);
-        return "deleted";
-    }
-}
-
-/**
- * 
- * @param {string} roomId group current id
- * @param {string} voterId voter number/id
- * @param {"up"|"down"|"delete"} status status
- */
-const updateVote = (roomId, voterId, status) => {
-    let $data = voteMap.get(roomId);
-    if (status === "up") {
-        if ($data["down"].includes(voterId)) {
-            $data["down"].splice($data["down"].indexOf(voterId), 1);
-            $data["voted"].push(voterId);
-            // update data
-            voteMap.set(roomId, $data);
-            $data = null;
-            return "added";
-        }
-        else if (!$data["voted"].includes(voterId)) {
-            $data["voted"].push(voterId);
-            voteMap.set(roomId, $data);
-            $data = null;
-            return "added";
-        }
-        else if ($data["voted"].includes(voterId)) {
-            return "voted";
-        }
-    }
-    else if (status === "down") {
-        if ($data["voted"].includes(voterId)) {
-            $data["voted"].splice(
-                $data["voted"].indexOf(voterId),
-                1
-            );
-            $data["down"].push(voterId);
-            // update data
-            voteMap.set(roomId, $data);
-            $data = null;
-            return "vote_down";
-        }
-        else if (!$data["down"].includes(voterId)) {
-            $data["down"].push(voterId);
-            voteMap.set(roomId, $data);
-            $data = null;
-            return "vote_down";
-        }
-        else if ($data["down"].includes(voterId)) {
-            return "down_voted";
-        }
-    }
-    else if (status === "delete") {
-        if ($data["voted"].includes(voterId)) {
-            $data["voted"].splice(
-                $data["voted"].indexOf(voterId),
-                1
-            );
-            voteMap.set(roomId, $data);
-            $data = null;
-            return "vote_del";
-        }
-        else if ($data["down"].includes(voterId)) {
-            $data["down"].splice(
-                $data["down"].indexOf(voterId),
-                1
-            );
-            voteMap.set(roomId, $data);
-            $data = null;
-            return "vote_del";
-        }
-        else if (
-            !$data["voted"].includes(voterId) &&
-            !$data["down"].includes(voterId)
-        ) {
-            return "no_data";
         }
     }
 }
---
+++ event/database/votes.js
@@ -0,0 +1,129 @@
+let voteMap = new Map();
+let $data = null;
+
+class Votes {
+
+    constructor() { }
+
+    /**
+     * Create new vote
+     * @param {string} roomId group id
+     * @param {string} owner sender id
+     */
+    create(roomId, owner, name = "voting") {
+        if (name === "") name = "voting";
+        voteMap.set(roomId, { name, owner, upvote: [], downvote: [] });
+    }
+
+    /**
+     * Delete vote
+     * @param {string} roomId group id
+     * @param {string} owner sender id
+     * @returns {boolean}
+     */
+    delete(roomId, owner) {
+        $data = voteMap.get(roomId);
+        
+        if (owner !== $data.owner) {
+            $data = null;
+            return false;
+        } else {
+            voteMap.delete(roomId);
+            $data = null;
+            return true;
+        }
+    }
+
+    /**
+     * Get current vote data
+     * @param {string} roomId group id
+     */
+    show(roomId) {
+        $data = voteMap.get(roomId);
+        let result_data = { present: false, data: null };
+        if (!$data) { return result_data; }
+        else {
+            result_data.present = true,
+                result_data.data = $data;
+            $data = null;
+            return result_data;
+        }
+    }
+
+    /**
+     * Upvote
+     * @param {string} roomId group id
+     * @param {string} participant sender id
+     * @returns {"added"|"voted"}
+     */
+    upvote(roomId, participant) {
+        $data = voteMap.get(roomId);
+        if ($data.downvote.includes(participant)) {
+            $data.downvote.splice($data.downvote.indexOf(participant), 1);
+            $data.upvote.push(participant);
+            voteMap.set(roomId, $data);
+            $data = null;
+            return "added";
+        } else if (!$data.upvote.includes(participant)) {
+            $data.upvote.push(participant);
+            voteMap.set(roomId, $data);
+            $data = null;
+            return "added";
+        } else {
+            return "voted";
+        }
+    }
+
+    /**
+     * Downvote
+     * @param {string} roomId group id
+     * @param {string} participant sender id
+     * @returns {"added"|"downvoted"}
+     */
+    downvote(roomId, participant) {
+        $data = voteMap.get(roomId);
+        
+        if ($data.upvote.includes(participant)) {
+            $data.upvote.splice($data.upvote.indexOf(participant), 1);
+            $data.downvote.push(participant);
+            voteMap.set(roomId, $data);
+            $data = null;
+            return "added";
+        } else if (!$data.downvote.includes(participant)) {
+            $data.downvote.push(participant);
+            voteMap.set(roomId, $data);
+            $data = null;
+            return "added";
+        } else {
+            $data = null;
+            return "downvoted";
+        }
+    }
+
+    /**
+     * Delete sender vote
+     * @param {string} roomId group id
+     * @param {string} participant sender id
+     * @returns {"deleted"|"no_vote"}
+     */
+    delete_vote(roomId, participant) {
+        $data = voteMap.get(roomId);
+        
+        if ($data.upvote.includes(participant)) {
+            $data.upvote.splice($data.upvote.indexOf(participant), 1);
+            voteMap.set(roomId, $data);
+            $data = null;
+            return "deleted";
+        } else if ($data.downvote.includes(participant)) {
+            $data.downvote.splice($data.downvote.indexOf(participant), 1);
+            voteMap.set(roomId, $data);
+            $data = null;
+            return "deleted";
+        } else {
+            $data = null;
+            return "no_vote";
+        }
+    }
+}
+
+module.exports = Votes;
