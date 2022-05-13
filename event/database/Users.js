3d8c6d8731c25972cc9bf2148fe3fc3d43b0ce79
FaizBastomi <64179402+FaizBastomi@users.noreply.github.com>
2022-05-13 21:20:43 +0700

    fix: Windows file path

--- event/database/Users.js
+++ event/database/Users.js
@@ -1,4 +1,5 @@
 const { promises, existsSync } = require("fs");
+const { join, basename } = require("path");
 const { Collection } = require("../../lib/Collection");
 const { color } = require("../../utils");
 const usersMap = new Collection();
@@ -23,21 +24,17 @@ class Users {
 				info: { age: null, adult: false },
 			};
 			usersMap.set(id, data);
-			print(`Success adding user ${id}`);
 			return true;
 		} catch (e) {
-			print(`Failed adding user ${id}`);
 			throw e;
 		}
 	}
 	deleteUser(id) {
 		if (!usersMap.has(id)) {
-			print(`'${id}' not found in database`);
 			return false;
 		}
 		try {
 			usersMap.delete(id);
-			print(`Success delete '${id}'`);
 			return true;
 		} catch (e) {
 			throw e;
@@ -54,13 +51,11 @@ class Users {
 		}
 	) {
 		if (!usersMap.has(id)) {
-			print(`'${id}' not found in database`);
 			return false;
 		}
 		try {
 			data = { id, ...data };
 			usersMap.set(id, data);
-			print(`Success edit '${id}' data`);
 			return true;
 		} catch (e) {
 			throw e;
@@ -68,7 +63,6 @@ class Users {
 	}
 	getUser(id) {
 		if (!usersMap.has(id)) {
-			print(`'${id}' not found in database`);
 			return false;
 		}
 		return usersMap.get(id);
@@ -76,16 +70,17 @@ class Users {
 	toArray() {
 		return Array.from(usersMap.values());
 	}
-	async writeToFile(path) {
-		if (!path) print(`'path' is empty`);
-		await promises.writeFile(path, JSON.stringify(this.toArray(usersMap), null, "\t"));
-		print("write user db to: " + path);
+	async writeToFile(filename) {
+		let $path = join(__dirname, "users", filename ? filename : "users-db.json");
+		if (!filename) print(`'filename' is empty defaulting to '${basename($path)}'`);
+		await promises.writeFile($path, JSON.stringify(this.toArray(usersMap), null, "\t"));
+		print("write user db to: " + $path);
 	}
-	async readFromFile(path) {
-		if (!path || !existsSync(path)) return `'path' is empty or not found`;
-		if (existsSync(path)) {
-			print("read user db from: " + path);
-			const dbStr = await promises.readFile(path, { encoding: "utf-8" });
+	async readFromFile(filename) {
+		let $path = join(__dirname, "users", filename ? filename : "users-db.json");
+		if (existsSync($path)) {
+			print("read user db from: " + $path);
+			const dbStr = await promises.readFile($path, { encoding: "utf-8" });
 			const dbJson = JSON.parse(dbStr);
 			if (Array.isArray(dbJson)) {
 				for (let dbX of dbJson) {
